36222496c0c21d02ca41ab1774a4c39760f60514
srinidhi2010 <109321775+srinidhi2010@users.noreply.github.com>
2023-09-24 14:02:29 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function fruit(){
+    window.location="fruits.html";
+}
+function bedroom(){
+    window.location="bedroom.html";
+}
+function desk(){
+    window.location="desk.html";
+}
+function backyard(){
+    window.location="backyard.html";
+}
+function road(){
+    window.location="road.html";
+}
